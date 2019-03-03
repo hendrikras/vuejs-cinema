48c9de9aedfe8e9c4286957d524e65c5f3d58ccd
Hendrik <hendrik@reinvent.nl>
2019-03-03 20:03:03 +0100

    add routes

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@ import './style.scss';
 import Overview from './components/Overview.vue';
 
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 
-Vue.use(VueResource);
+import routes from './util/routes'
 
 import moment from 'moment-timezone';
 import { checkFilter } from './util/bus';
 // moment.
+
+Vue.use(VueResource);
+const router = new VueRouter({ routes });
+Vue.use(VueRouter);
 
 Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
 Object.defineProperty(Vue.prototype, '$bus', { get() { return this.$root.bus } });
@@ -36,5 +41,6 @@ new Vue({
   const that = checkFilter.bind(this);
   console.log(6, that);
   this.$bus.$on('check-filter', that);
-  }
+  },
+  router
 });
